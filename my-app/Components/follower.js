aed4692bb8b4b947e8224e4f8cbb969b9de0b237
rongni <rni4@illinois.edu>
2021-03-22 23:49:48 -0500

    fix eslint

--- my-app/Components/follower.js
+++ my-app/Components/follower.js
@@ -1,18 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { Text, View } from 'react-native';
 
-
-
-
-const Follower = () => {
-  console.log('follower rendered!!')
-  return (
-    <View style={{flex: 1}}>
-        <View>
-        <Text>Follower is here</Text>
-        </View>
+const Follower = () => (
+  <View style={{ flex: 1 }}>
+    <View>
+      <Text>Follower is here</Text>
     </View>
-  )
-}
+  </View>
+);
 
-export default Follower
+export default Follower;
--- my-app/Components/following.js
+++ my-app/Components/following.js
@@ -1,18 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { Text, View } from 'react-native';
 
-
-
-
-const Following = () => {
-  console.log('following rendered!!')
-  return (
-    <View style={{flex: 1}}>
-        <View>
-        <Text>Following is here</Text>
-        </View>
+const Following = () => (
+  <View style={{ flex: 1 }}>
+    <View>
+      <Text>Following is here</Text>
     </View>
-  )
-}
+  </View>
+);
 
-export default Following
+export default Following;
